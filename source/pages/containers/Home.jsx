2ca5a613651d3e87049a233bd8e3a11ce8d6b740
Miguel Romero <ingmromero34@gmail.com>
2017-04-30 09:41:23 -0500

    home

--- source/pages/containers/Home.jsx
+++ source/pages/containers/Home.jsx
@@ -3,6 +3,8 @@ import { FormattedMessage } from 'react-intl';
 
 import Title from '../../shared/components/Title';
 
+import css from './Page.css';
+
 function Home() {
   return (
     <div name="Home" className="section no-pad-bot" id="index-banner">
@@ -20,7 +22,46 @@ function Home() {
         <br /><br />
       </div>
     </div>
-  );
+
+    <div className="container">
+      <div className="section">
+        <div className="row">
+          <div className="col s12 m4">
+            <div className={css.icon-block}>
+              <h2 className="center light-blue-text"><i className={css.material-icons}>flash_on</i></h2>
+              <h5 className="center">Speeds up development</h5>
+
+              <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+            </div>
+          </div>
+
+          <div className="col s12 m4">
+            <div className={css.icon-block}>
+              <h2 className="center light-blue-text"><i className={css.material-icons}>group</i></h2>
+              <h5 className="center">User Experience Focused</h5>
+
+              <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+            </div>
+          </div>
+
+          <div className="col s12 m4">
+            <div className={css.icon-block}>
+              <h2 className="center light-blue-text"><i className={css.material-icons}>settings</i></h2>
+              <h5 className="center">Easy to work with</h5>
+
+              <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <br /><br />
+
+      <div class="section">
+
+      </div>
+    </div>
+    );
 }
 
 export default Home;
