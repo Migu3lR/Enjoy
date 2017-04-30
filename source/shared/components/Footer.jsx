8fda60bbd0142ea5d89727e4dd75d0ab26385594
Miguel Romero <ingmromero34@gmail.com>
2017-04-30 18:01:56 -0500

    footer

--- source/shared/components/Footer.jsx
+++ source/shared/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import css from './Footer.css';
+import './Footer.css';
 
 function Footer() {
   return (
     <footer className="page-footer">
       <div className="footer-copyright">
         <div className="container">
-          <p className="center white-text">Enjoy Life ProNET is <i className={`material-icons ${css.material_icons}`}>code</i> by <a href="https://migu3lr.github.io/">Miguel Romero</a></p>
+          <p className="center white-text">Enjoy Life ProNET is &lt; / &gt; by <a href="https://migu3lr.github.io/">Miguel Romero</a></p>
           <p className="center grey-text text-lighten-4">Coaching Alegra © 2017</p>
         </div>
       </div>
