15d84d26ae5d0f88ea197c25108abef30601d40e
Miguel Romero <ingmromero34@gmail.com>
2017-04-30 02:42:44 -0500

    header

--- source/shared/components/Header.jsx
+++ source/shared/components/Header.jsx
@@ -2,35 +2,34 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <nav className="light-blue lighten-1" role="navigation">
-          <div className="nav-wrapper container">
-            <a id="logo-container" className="brand-logo">Enjoy Life</a>
-            <ul className="right hide-on-med-and-down">
-              <li>
-                <Link to="/">
-                  <FormattedMessage id="header.nav.home" />
-                </Link>
-              </li>
-            </ul>
+function Header() {
+  return (
+    <header>
+      <nav className="light-blue lighten-1" role="navigation">
+        <div className="nav-wrapper container">
+          <a id="logo-container" className="brand-logo">Enjoy Life</a>
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <Link to="/">
+                <FormattedMessage id="header.nav.home" />
+              </Link>
+            </li>
+          </ul>
 
-            <ul id="nav-mobile" className="side-nav">
-              <li>
-                <Link to="/">
-                  <FormattedMessage id="header.nav.home" />
-                </Link>
-              </li>
-            </ul>
-            <a href="#m" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
-          </div>
-        </nav>
-      </header >
-    );
-  }
-
+          <ul id="nav-mobile" className="side-nav">
+            <li>
+              <Link to="/">
+                <FormattedMessage id="header.nav.home" />
+              </Link>
+            </li>
+          </ul>
+          <a href="#m" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
+        </div>
+      </nav>
+    </header >
+  );
 }
+
+
 
 export default Header;
