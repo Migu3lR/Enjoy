19de2697e33bbdf5facf1d1c28c1b2514fcd4b74
Miguel Romero <ingmromero34@gmail.com>
2017-04-30 02:27:58 -0500

    header

--- source/shared/components/Header.jsx
+++ source/shared/components/Header.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
+import css from './Header.css';
+
 class Header extends Component {
   componentDidMount() {
     this.button_collapse.sideNav();
@@ -10,35 +12,35 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <nav className="white" role="navigation">
+        <nav className="light-blue lighten-1" role="navigation">
           <div className="nav-wrapper container">
             <a id="logo-container" className="brand-logo">Enjoy Life</a>
-            <ul className="right hide-on-med-and-down">
-              <li>
-                <Link to="/">
-                  <FormattedMessage id="header.nav.home" />
-                </Link>
-              </li>
-            </ul>
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <Link to="/">
+                <FormattedMessage id="header.nav.home" />
+              </Link>
+            </li>
+          </ul>
 
-            <ul id="nav-mobile" className="side-nav">
-              <li>
-                <Link to="/">
-                  <FormattedMessage id="header.nav.home" />
-                </Link>
-              </li>
-            </ul>
-            <a
-              href="//" data-activates="nav-mobile" className="button-collapse"
-              ref={
-                //eslint-disable-next-line
-                node => this.button_collapse = node
-                //eslint-disable-next-line
-              }>
-              <i className="material-icons">menu</i></a>
+          <ul id="nav-mobile" className="side-nav">
+            <li>
+              <Link to="/">
+                <FormattedMessage id="header.nav.home" />
+              </Link>
+            </li>
+          </ul>
+          <a
+            href="//" data-activates="nav-mobile" className="button-collapse"
+            ref={
+              //eslint-disable-next-line
+              node => this.button_collapse = node
+              //eslint-disable-next-line
+            }>
+            <i className="material-icons">menu</i></a>
           </div>
         </nav>
-      </header>
+      </header >
     );
   }
 
