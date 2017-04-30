8ac4d44f5c722fe8c22cf37f6d9288d3345f979d
Miguel Romero <ingmromero34@gmail.com>
2017-04-30 01:29:53 -0500

    pruebas materialze

--- source/pages/components/Layout.jsx
+++ source/pages/components/Layout.jsx
@@ -1,48 +1,36 @@
-import React, { PropTypes, Component } from 'react';
-import { findDOMNode } from 'react-dom';
+import React, { PropTypes } from 'react';
 import $ from 'jquery';
 
-class Layout extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    const bc = findDOMNode(this.refs.butcol);
-    $(bc).sideNav();
-  }
-
-  render() {
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <title>{props.title}</title>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
-          />
-          <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
-          <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-          <link type="text/css" rel="stylesheet" href={`${props.domain}/materialize.css`} media="screen,projection" />
-          <link rel="stylesheet" href={`${props.domain}/styles.css`} />
-        </head>
-        <body>
-          <div
-            id="render-target"
-            dangerouslySetInnerHTML={{
-              __html: props.content,
-            }}
-          />
-          <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js" />
-          <script type="text/javascript" src={`${props.domain}/materialize.js`} />
-          <script src={`${props.domain}/app.js`} />
-
-        </body>
-      </html>
-    );
-  }
+function Layout(props) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>{props.title}</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
+        />
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        <link type="text/css" rel="stylesheet" href={`${props.domain}/materialize.css`} media="screen,projection" />
+        <link rel="stylesheet" href={`${props.domain}/styles.css`} />
+      </head>
+      <body>
+        <div
+          id="render-target"
+          dangerouslySetInnerHTML={{
+            __html: props.content,
+          }}
+        />
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js" />
+        <script type="text/javascript" src={`${props.domain}/materialize.js`} />
+        <script src={`${props.domain}/app.js`} />
+      </body>
+    </html>
+  );
 }
+
 
 Layout.propTypes = {
   title: PropTypes.string,
--- source/shared/components/Header.jsx
+++ source/shared/components/Header.jsx
@@ -2,32 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
-function Header() {
-  return (
-    <header>
-      <nav className="white" role="navigation">
-        <div className="nav-wrapper container">
-          <a id="logo-container" className="brand-logo">Enjoy Life</a>
-          <ul className="right hide-on-med-and-down">
-            <li>
-              <Link to="/">
-                <FormattedMessage id="header.nav.home" />
-              </Link>
-            </li>
-          </ul>
+class Header extends Component {
+  componentDidMount() {
+    this.button_collapse.sideNav();
+  }
 
-          <ul id="nav-mobile" className="side-nav">
-            <li>
-              <Link to="/">
-                <FormattedMessage id="header.nav.home" />
-              </Link>
-            </li>
-          </ul>
-          <a href="//" data-activates="nav-mobile" className="button-collapse" ref='butcol'><i className="material-icons">menu</i></a>
-        </div>
-      </nav>
-    </header>
-  );
+  render() {
+    return (
+      <header>
+        <nav className="white" role="navigation">
+          <div className="nav-wrapper container">
+            <a id="logo-container" className="brand-logo">Enjoy Life</a>
+            <ul className="right hide-on-med-and-down">
+              <li>
+                <Link to="/">
+                  <FormattedMessage id="header.nav.home" />
+                </Link>
+              </li>
+            </ul>
+
+            <ul id="nav-mobile" className="side-nav">
+              <li>
+                <Link to="/">
+                  <FormattedMessage id="header.nav.home" />
+                </Link>
+              </li>
+            </ul>
+            <a href="//" data-activates="nav-mobile" className="button-collapse" ref={node => this.button_collapse = node}><i className="material-icons">menu</i></a>
+          </div>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Header;
