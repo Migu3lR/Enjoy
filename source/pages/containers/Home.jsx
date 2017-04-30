a163e763da6433db252153f038f980c671a926f3
Miguel Romero <ingmromero34@gmail.com>
2017-04-30 16:18:13 -0500

    home

--- source/pages/containers/Home.jsx
+++ source/pages/containers/Home.jsx
@@ -23,10 +23,10 @@ function Home() {
               <p className="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               <div className="row">
                 <div className="col s6">
-                  <a class="waves-effect waves-light btn orange">Colabora</a>
+                  <a className="waves-effect waves-light btn orange">Colabora</a>
                 </div>
                 <div className="col s6">
-                  <a class="waves-effect waves-light btn orange">Ir a Enjoy Life</a>
+                  <a className="waves-effect waves-light btn orange">Ir a Enjoy Life</a>
                 </div>
               </div>
             </div>
