1b30c7eda775260987499bcd54a520ee75cf180e
Miguel Romero <ingmromero34@gmail.com>
2017-04-30 16:53:52 -0500

    home center

--- source/pages/containers/Home.jsx
+++ source/pages/containers/Home.jsx
@@ -38,8 +38,8 @@ function Home() {
         <div className="section">
           <div className={`row ${css.section2}`}>
             <div className="col s12 m10 offset-m1">
-              <p className={`flow-text ${css.p_dona}`}>Conoce el proyecto Enjoy Life ProNET, puedes colaborar con una donacion. Clic en el siguiente boton para donar.</p>
-              <a className="waves-effect waves-light btn">Ir a Enjoy Life</a>
+              <p className={`flow-text center-align ${css.p_dona}`}>Conoce el proyecto Enjoy Life ProNET, puedes colaborar con una donacion. Clic en el siguiente boton para donar.</p>
+              <a className="waves-effect waves-light btn center-align">Ir a Enjoy Life</a>
             </div>
           </div>
           <div className="row">
