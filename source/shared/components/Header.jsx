781aeeaf27d0149ad0c8b44a752781a029e226a0
Miguel Romero <ingmromero34@gmail.com>
2017-05-02 23:18:26 -0500

    menus header login

--- source/shared/components/Header.jsx
+++ source/shared/components/Header.jsx
@@ -30,24 +30,46 @@ class Header extends Component {
         <nav className="white" role="navigation">
           <div className="nav-wrapper container">
             <a id="logo-container" className={`brand-logo ${css.brand_logo}`}>Enjoy Life ProNET</a>
+            {/* Menu navegacion en desktop */}
             <ul className="right hide-on-med-and-down">
+              {/* Menu en Enjoy */}
+              {this.state.PrintLogin && (
               <li>
                 <Link to="/">
                   <FormattedMessage id="header.nav.home" />
                 </Link>
               </li>
+              )}
+              {/* Menu en Home */}
+              {!this.state.PrintLogin && (
+              <li>
+                <Link to="/">
+                  <FormattedMessage id="header.nav.home" />
+                </Link>
+              </li>
+              )}
             </ul>
+
+            {/* Menu navegacion en mobiles */}
             <ul id="nav-mobile" className="side-nav">
+              {/* Menu en Enjoy */}
+              {this.state.PrintLogin && (
               <li>
                 <Link to="/">
                   <FormattedMessage id="header.nav.home" />
                 </Link>
               </li>
+              )}
+              {/* Menu en Home */}
+              {!this.state.PrintLogin && (
+              <li>
+                <Link to="/">
+                  <FormattedMessage id="header.nav.home" />
+                </Link>
+              </li>
+              )}
             </ul>
             <a href="#!" data-activates="nav-mobile" className={`button-collapse ${css.button_collapse}`}><i className="material-icons">menu</i></a>
-            {this.state.PrintLogin && (
-              <span>LOGIN</span>
-            )}
           </div>
         </nav>
       </header>
