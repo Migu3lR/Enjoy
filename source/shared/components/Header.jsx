2d6dc0b82fb686e10a06fa0c3332c4eacc62af49
Miguel Romero <ingmromero34@gmail.com>
2017-05-03 07:35:51 -0500

    props header

--- source/shared/components/Header.jsx
+++ source/shared/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
