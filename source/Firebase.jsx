4b3a94c1ae2e953509bde50f57f1222d727f5e46
Miguel Romero <ingmromero34@gmail.com>
2017-05-03 22:40:21 -0500

    probando firebase

--- source/Firebase.jsx
+++ source/Firebase.jsx
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 import serviceAccount from './EnjoyLife-d535250a0bd1.json';
 
-const fapp = firebase.initializeApp({
+firebase.initializeApp({
   serviceAccount,
   databaseURL: 'https://enjoylife-32afb.firebaseio.com',
 });
 
-const db = fapp.database();
+const db = firebase.database();
 
 export default db;
