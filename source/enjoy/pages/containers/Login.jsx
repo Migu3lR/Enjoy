e9afe9af424154b3751356e6d3430e9cec5682a2
Miguel Romero <ingmromero34@gmail.com>
2017-05-04 18:43:21 -0500

    login

--- source/enjoy/pages/containers/Login.jsx
+++ source/enjoy/pages/containers/Login.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <section name='login'>
-      <div className='row'>
-        <div className='col s12 m7'>
-          <div className='card'>
-            <span className='card-title'>Card Title</span>
-            <div className='card-content'>
-              <div className='row'>
-                <div className='input-field col s12'>
-                  <input id='email' type='email' className='validate' />
-                  <label htmlFor='email'>Correo Electrónico</label>
+    <section name="login">
+      <div className="row">
+        <div className="col s12 m7">
+          <div className="card">
+            <span className="card-title">Card Title</span>
+            <div className="card-content">
+              <div className="row">
+                <div className="input-field col s12">
+                  <input id="email" type="email" className="validate" />
+                  <label htmlFor="email">Correo Electrónico</label>
                 </div>
               </div>
-              <div className='row'>
-                <div className='input-field col s12'>
-                  <input id='password' type='password' className='validate' />
-                  <label htmlFor='password'>Contraseña</label>
+              <div className="row">
+                <div className="input-field col s12">
+                  <input id="password" type="password" className="validate" />
+                  <label htmlFor="password">Contraseña</label>
                 </div>
               </div>
             </div>
-            <div className='card-action'>
-              ¿Aún no tienes un cuenta? <Link to='/enjoy/register/email'>Regístrate aquí</Link>
+            <div className="card-action">
+              ¿Aún no tienes un cuenta? <Link to="/enjoy/register/email">Regístrate aquí</Link>
             </div>
           </div>
         </div>
