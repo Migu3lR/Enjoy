dd065247d7812b76f77dd3044edaa9c6bc0ffbd4
Miguel Romero <ingmromero34@gmail.com>
2017-05-03 23:54:22 -0500

    kkk

--- source/Firebase.jsx
+++ source/Firebase.jsx
@@ -1,9 +1,12 @@
 import * as firebase from 'firebase';
-import serviceAccount from './EnjoyLife-d535250a0bd1.json';
 
 const firebaseApp = firebase.initializeApp({
-  serviceAccount,
+  apiKey: 'AIzaSyBgPTYEOEdRan943EQJ4PjDoIKJLnah0qA',
+  authDomain: 'enjoylife-32afb.firebaseapp.com',
   databaseURL: 'https://enjoylife-32afb.firebaseio.com',
+  projectId: 'enjoylife-32afb',
+  storageBucket: 'enjoylife-32afb.appspot.com',
+  messagingSenderId: '153966974380'
 });
 
 export default firebaseApp;
