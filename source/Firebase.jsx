7cac8591a558c289ecd7260dfaaf6b10c1cf9358
Miguel Romero <ingmromero34@gmail.com>
2017-05-03 22:53:52 -0500

    probando firebase

--- source/Firebase.jsx
+++ source/Firebase.jsx
@@ -1,11 +1,12 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import serviceAccount from './EnjoyLife-d535250a0bd1.json';
 
-firebase.initializeApp({
+const firebaseApp = firebase.initializeApp({
   serviceAccount,
   databaseURL: 'https://enjoylife-32afb.firebaseio.com',
 });
 
-const db = firebase.database();
+const db = firebaseApp.database();
 
 export default db;
