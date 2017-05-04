90fbfca62c4782b69788e4657306f89796a68d8b
Miguel Romero <ingmromero34@gmail.com>
2017-05-03 23:03:22 -0500

    pruebas firebase

--- source/Firebase.jsx
+++ source/Firebase.jsx
@@ -1,12 +1,11 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+import * as firebase from 'firebase';
 import serviceAccount from './EnjoyLife-d535250a0bd1.json';
 
 const firebaseApp = firebase.initializeApp({
   serviceAccount,
   databaseURL: 'https://enjoylife-32afb.firebaseio.com',
 });
-
+console.log(firebaseApp);
 const db = firebaseApp.database();
 
 export default db;
