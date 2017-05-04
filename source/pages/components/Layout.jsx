6538edd67f469639ee7b28dfdc7ef0d1c0762b7c
Miguel Romero <ingmromero34@gmail.com>
2017-05-03 21:50:10 -0500

    prueba firebase

--- source/pages/components/Layout.jsx
+++ source/pages/components/Layout.jsx
@@ -25,10 +25,12 @@ function Layout(props) {
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js" />
         <script type="text/javascript" src={`${props.domain}/materialize.js`} />
         <script type="text/javascript">
-          firebase.initializeApp({{
-            serviceAccount,
-            databaseURL: 'https://enjoylife-32afb.firebaseio.com',
-          }});
+          {
+            firebase.initializeApp({
+              serviceAccount,
+              databaseURL: 'https://enjoylife-32afb.firebaseio.com',
+            })
+          }
         </script>
         <script src={`${props.domain}/app.js`} />
         <script type="text/javascript" src={`${props.domain}/initialize.js`} />
