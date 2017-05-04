dbc7b532290e372a07a9ad4b55202124b4bbf0db
Miguel Romero <ingmromero34@gmail.com>
2017-05-03 21:45:56 -0500

    prueba firebase

--- built/statics/initialize.js
+++ built/statics/initialize.js
@@ -1,11 +1,3 @@
-import firebase from 'firebase';
-import serviceAccount from '../../source/EnjoyLife-d535250a0bd1.json';
-
-firebase.initializeApp({
-  serviceAccount,
-  databaseURL: 'https://enjoylife-32afb.firebaseio.com',
-});
-
 if (typeof (jQuery) === 'undefined') {
   let jQuery
 
--- source/pages/components/Layout.jsx
+++ source/pages/components/Layout.jsx
@@ -1,4 +1,6 @@
 import React, { PropTypes } from 'react';
+import firebase from 'firebase';
+import serviceAccount from '../../EnjoyLife-d535250a0bd1.json';
 
 function Layout(props) {
   return (
@@ -22,6 +24,12 @@ function Layout(props) {
         }<div id="render-target" dangerouslySetInnerHTML={{ __html: props.content }} />
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js" />
         <script type="text/javascript" src={`${props.domain}/materialize.js`} />
+        <script type="text/javascript">
+          firebase.initializeApp({
+            serviceAccount,
+            databaseURL: 'https://enjoylife-32afb.firebaseio.com',
+          });
+        </script>
         <script src={`${props.domain}/app.js`} />
         <script type="text/javascript" src={`${props.domain}/initialize.js`} />
       </body>
