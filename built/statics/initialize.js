0b8cbb25021fde7e3534eac6f26fc459702c292e
Miguel Romero <ingmromero34@gmail.com>
2017-05-04 21:57:07 -0500

    eslint

--- built/statics/initialize.js
+++ built/statics/initialize.js
@@ -1,26 +1,25 @@
 if (typeof (jQuery) === 'undefined') {
-  let jQuery
+  let jQuery;
 
   if (typeof (require) === 'function') {
     jQuery = $ = require('jquery');
-
   } else {
     jQuery = $;
   }
 }
-;
+
 (function ($) {
   // INITIALIZE
   $('.button-collapse').sideNav();
   $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
-    }
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false, // Stops event propagation
+  },
   );
 }(jQuery));
