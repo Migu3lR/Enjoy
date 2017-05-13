28dd735bfb01411bfd4364e6107e75bec84c4c68
Miguel Romero <ingmromero34@gmail.com>
2017-05-13 01:23:26 -0500

    pprbando

--- source/enjoy/pages/containers/Portal.jsx
+++ source/enjoy/pages/containers/Portal.jsx
@@ -12,16 +12,16 @@ function Portal(props) {
         <input name="merchantId" type="hidden" value="508029" />
         <input name="accountId" type="hidden" value="512321" />
         <input name="description" type="hidden" value="Test PAYU" />
-        <input name="referenceCode" type="hidden" value="ALG-00003" />
+        <input name="referenceCode" type="hidden" value="ALG-00004" />
         <input name="amount" type="hidden" value="10000" />
         <input name="tax" type="hidden" value="0" />
         <input name="taxReturnBase" type="hidden" value="0" />
         <input name="currency" type="hidden" value="COP" />
-        <input name="signature" type="hidden" value="1ed813dd6f77e7350e520c3f6a0ea717" />
+        <input name="signature" type="hidden" value="3e74bfda62b4581ea7a339fd75505404" />
         <input name="test" type="hidden" value="1" />
         <input name="buyerEmail" type="hidden" value="ingmromero34@gmail.com" />
-        <input name="responseUrl" type="hidden" value="http://alegratuvida.com/enjoy/portal/transaccion" />
-        <input name="confirmationUrl" type="hidden" value="http://alegratuvida.com:55880" />
+        <input name="responseUrl" type="hidden" value="http://alegratuvida.com/enjoy/portal/" />
+        <input name="confirmationUrl" type="hidden" value="http://alegratuvida.com:55880/payu" />
         <input name="Submit" type="submit" value="Enviar" />
       </form>
     </section>
