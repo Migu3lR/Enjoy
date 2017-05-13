cc780f0b2f039b9bb6f125e2ead43237daf96844
Miguel Romero <ingmromero34@gmail.com>
2017-05-13 00:56:13 -0500

    pruebaas

--- source/payu-listener.jsx
+++ source/payu-listener.jsx
@@ -1,25 +1,14 @@
-import * as firebase from 'firebase';
-
 import express from 'express';
 import bodyParser from 'body-parser';
-
-const Firebase = firebase.initializeApp({
-  apiKey: 'AIzaSyBgPTYEOEdRan943EQJ4PjDoIKJLnah0qA',
-  authDomain: 'enjoylife-32afb.firebaseapp.com',
-  databaseURL: 'https://enjoylife-32afb.firebaseio.com',
-  projectId: 'enjoylife-32afb',
-  storageBucket: 'enjoylife-32afb.appspot.com',
-  messagingSenderId: '153966974380',
-});
-
-
+import firebase from 'firebase';
+import Firebase from './Firebase';
 
 const app = express();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-const token = Firebase.auth().createCustomToken('alegraEL_Payments', { unlimited: true });
+const token = firebase.auth().createCustomToken('alegraEL_Payments', { unlimited: true });
 
 let auth = false;
 
