d9e7eb5ae6b6405e158be9cb088cecffb13c43d5
Miguel Romero <ingmromero34@gmail.com>
2017-05-13 00:29:08 -0500

    pruebas

--- source/payu-listener.jsx
+++ source/payu-listener.jsx
@@ -1,17 +1,29 @@
+import * as firebase from 'firebase';
+
 import express from 'express';
 import bodyParser from 'body-parser';
-import Services from './Services';
+
+const Firebase = firebase.initializeApp({
+  apiKey: 'AIzaSyBgPTYEOEdRan943EQJ4PjDoIKJLnah0qA',
+  authDomain: 'enjoylife-32afb.firebaseapp.com',
+  databaseURL: 'https://enjoylife-32afb.firebaseio.com',
+  projectId: 'enjoylife-32afb',
+  storageBucket: 'enjoylife-32afb.appspot.com',
+  messagingSenderId: '153966974380',
+});
+
+
 
 const app = express();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-const token = Services.Auth.createCustomToken('alegraEL_Payments', { unlimited: true });
+const token = Firebase.auth().createCustomToken('alegraEL_Payments', { unlimited: true });
 
 let auth = false;
 
-const acceso = Services.Auth.signInWithCustomToken(token);
+const acceso = Firebase.auth().signInWithCustomToken(token);
 
 acceso.then(() => {
   auth = true;
