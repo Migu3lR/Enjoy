baa9490ab39746ecf78ea3dfa45f6c0af86c08c9
Miguel Romero <ingmromero34@gmail.com>
2017-05-14 01:54:27 -0500

    prueb payu confirm

--- source/payu-listener.jsx
+++ source/payu-listener.jsx
@@ -7,27 +7,38 @@ const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-let auth = false;
 
 const acceso = Firebase.auth().signInWithEmailAndPassword('alegraEL_Payments@alegraELPayments.com', 'alegraEL_Payments!"#');
-
-acceso.then(() => {
-  auth = true;
-  console.log('Login OK.');
-});
-
-acceso.catch(() => {
-  console.log('Login Error.');
-  auth = false;
-});
-
 
 app.post('/payu', (req, res) => {
   const params = req.body;
   console.log(params);
-  
-  res.writeHead(200);
-  res.end();
+
+  acceso.then(() => {
+    console.log('Login OK.');
+
+    const transaccion = {
+      Estado: params.state_pol,
+      EstadoDet: params.response_code_pol,
+    }
+
+    const updates = {};
+    updates[`/transacciones/${params.reference_sale}`] = transaccion;
+    updates[`/usuarios/${params.extra1}/transacciones/${params.reference_sale}`] = transaccion;
+
+    Data.ref().update(updates);
+    
+    firebase.database().ref(`/transacciones/${params.reference_sale}`)
+    .on('child_changed', () => {
+      res.writeHead(200);
+      res.end();
+  });
+
+  acceso.catch(() => {
+    console.log('Login Error.');
+    res.writeHead(400);
+    res.end();
+  });
 });
 
 app.listen(55880);
