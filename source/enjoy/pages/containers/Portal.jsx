2635cdf0200ea861c45145745ebef2d8e7119f9b
Miguel Romero <ingmromero34@gmail.com>
2017-05-14 01:13:16 -0500

    probando portal

--- source/enjoy/pages/containers/Portal.jsx
+++ source/enjoy/pages/containers/Portal.jsx
@@ -4,33 +4,46 @@ import { Link } from 'react-router-dom';
 import queryString from 'query-string';
 import api from '../../../api';
 
-function Portal(props) {
-  let trx;
-  api.db.nuevaTrx('Curso 1', 70000).then((d) => {
-    trx = d;
-  });
-  console.log(queryString.parse(props.location.search));
-  return (
-    <section name="Portal">
-      <form method="post" action={trx.api}>
-        <input name="merchantId" type="hidden" value={trx.merchantId} />
-        <input name="accountId" type="hidden" value={trx.accountId} />
-        <input name="description" type="hidden" value={trx.description} />
-        <input name="referenceCode" type="hidden" value={trx.referenceCode} />
-        <input name="amount" type="hidden" value={trx.amount} />
-        <input name="tax" type="hidden" value={trx.tax} />
-        <input name="taxReturnBase" type="hidden" value={trx.taxReturnBase} />
-        <input name="currency" type="hidden" value={trx.currency} />
-        <input name="signature" type="hidden" value={trx.signature} />
-        <input name="test" type="hidden" value="1" />
-        <input name="buyerEmail" type="hidden" value={trx.buyerEmail} />
-        <input name="extra1" type="hidden" value={trx.extra1} />
-        <input name="responseUrl" type="hidden" value="http://alegratuvida.com/enjoy/portal/" />
-        <input name="confirmationUrl" type="hidden" value="http://alegratuvida.com:55880/payu" />
-        <input name="Submit" type="submit" value="Enviar" />
-      </form>
-    </section>
-  );
+class Portal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTrx: {},
+    };
+    console.log(queryString.parse(props.location.search));
+  }
+
+  componentDidMount() {
+    api.db.nuevaTrx('Curso 1', 70000).then((newTrx) => {
+      this.setState({
+        newTrx,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <section name="Portal">
+        <form method="post" action={this.state.newTrx.api}>
+          <input name="merchantId" type="hidden" value={this.state.newTrx.merchantId} />
+          <input name="accountId" type="hidden" value={this.state.newTrx.accountId} />
+          <input name="description" type="hidden" value={this.state.newTrx.description} />
+          <input name="referenceCode" type="hidden" value={this.state.newTrx.referenceCode} />
+          <input name="amount" type="hidden" value={this.state.newTrx.amount} />
+          <input name="tax" type="hidden" value={this.state.newTrx.tax} />
+          <input name="taxReturnBase" type="hidden" value={this.state.newTrx.taxReturnBase} />
+          <input name="currency" type="hidden" value={this.state.newTrx.currency} />
+          <input name="signature" type="hidden" value={this.state.newTrx.signature} />
+          <input name="test" type="hidden" value="1" />
+          <input name="buyerEmail" type="hidden" value={this.state.newTrx.buyerEmail} />
+          <input name="extra1" type="hidden" value={this.state.newTrx.extra1} />
+          <input name="responseUrl" type="hidden" value="http://alegratuvida.com/enjoy/portal/" />
+          <input name="confirmationUrl" type="hidden" value="http://alegratuvida.com:55880/payu" />
+          <input name="Submit" type="submit" value="Enviar" />
+        </form>
+      </section>
+    );
+  }
 }
 
 export default Portal;
